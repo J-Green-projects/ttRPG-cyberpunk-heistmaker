013ac4a5286607ca027bf00f7a9dc3d94de0e349
J-Green-projects <jecgreenwood@gmail.com>
2025-08-14 12:54:31 -0400

    updated for readability

--- cyberpunkHeistGen.py
+++ cyberpunkHeistGen.py
@@ -6,6 +6,7 @@ import parsing as pr
 inputArgs = sys.argv[1:3] # argv[0] == cyberpunkHeistGen.py argv[1] == #+# argv[2] == str+str+str
 INDEX_PARAMETER_ARGS, INDEX_FILENAME_ARGS = 0, 1
 BASE_REWARD_VALUE_DEFAULT, REWARD_ROUNDING_VALUE_DEFAULT = 1500, 50 # average monthly individual wage and lowest increment the GM wants to assign reward $ in
+TYPICAL_NUM_HEISTERS = 3
 
 # default values
 filenames = ["corps", "jobs", "defenses"]
@@ -28,19 +29,18 @@ while (True):
     # combined difficulty score of the current job
     difficulty = int(selectedCorp.difficulty) + int(selectedJob.difficulty)
 
-    # factor of 0.9-3.2~, as jobs get exponentially more involved ($1,500 - $27,900 using default reward and rounding values).
-    reward = round((((difficulty/3)**2.25)*baseReward)/rewardRounding)*rewardRounding
+    # factor of 0.9-3.2~, as jobs get exponentially more involved ($1,500 - $27,900 using default reward and rounding values). 2.25 is a purely arbitrary number that produces a nice looking reward curve.
+    reward = round((((difficulty/TYPICAL_NUM_HEISTERS)**2.25)*baseReward)/rewardRounding)*rewardRounding
 
     # generate the balance of the types of defences, assuming equal priority.
     defensesToGen = [0] * len(defenseList)
     for i in range(difficulty):
         defensesToGen[randint(0,len(defenseList)-1)] += 1
 
-    # primary output
+    # Output
     print("Target: " + selectedCorp.name + "\nContract Type: " + selectedJob.name + 
           "\nDifficulty mod: " + str(difficulty) + "\nReward: $" + str(reward))
     
-    # create a string for 
     for i in range(len(defenseList)):
         print(defenseList[i].generateString(defensesToGen[i]))
 
